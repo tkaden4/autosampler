ab354de27d13281503bedb4082c3d113faae7aca
tkaden4 <thomas.kaden4@outlook.com>
2023-05-01 03:54:25 -0700

    Padding

--- src/main/java/Piano.java
+++ src/main/java/Piano.java
@@ -7,10 +7,11 @@ public class Piano {
   static int BLACK_KEY_WIDTH = (int) (WHITE_KEY_WIDTH / 1.71);
   static int BLACK_KEY_HEIGHT = (int) (WHITE_KEY_HEIGHT / 1.61);
   static int MARGIN = 2;
+  static int PADDING = 5;
   static int NUDGE = 1;
 
   static int width(int octaves) {
-    return MARGIN + (octaves * 7) * (WHITE_KEY_WIDTH + MARGIN) + MARGIN;
+    return PADDING + (octaves * 7) * (WHITE_KEY_WIDTH + MARGIN) + PADDING;
   }
 
   static Canvas create(int width, int height, int octaves) {
@@ -26,21 +27,21 @@ public class Piano {
     // draw the white keys
     ctx.setFill(Color.valueOf("#eaeaea"));
     for (int i = 0; i < whiteKeys; ++i) {
-      var x = MARGIN + i * (WHITE_KEY_WIDTH + MARGIN);
-      var y = MARGIN;
+      var x = PADDING + i * (WHITE_KEY_WIDTH + MARGIN);
+      var y = PADDING;
       ctx.fillRect(x, y, WHITE_KEY_WIDTH, WHITE_KEY_HEIGHT);
     }
 
     // draw the black keys
-    ctx.setFill(Color.BLACK);
+    ctx.setFill(Color.valueOf("#0a0a0c"));
 
     var blackKeyOffsets = new int[] { 0, 1, 3, 4, 5 };
     var blackKeyNudge = new int[] { -NUDGE, NUDGE, -NUDGE, 0, NUDGE };
     for (int i = 0; i < octaves; ++i) {
       for (int offset = 0; offset < blackKeyOffsets.length; ++offset) {
-        var x = MARGIN + (i * (WHITE_KEY_WIDTH + MARGIN) * 7) + (blackKeyOffsets[offset] * (WHITE_KEY_WIDTH + MARGIN))
+        var x = PADDING + (i * (WHITE_KEY_WIDTH + MARGIN) * 7) + (blackKeyOffsets[offset] * (WHITE_KEY_WIDTH + MARGIN))
             + (WHITE_KEY_WIDTH + MARGIN) - ((BLACK_KEY_WIDTH) / 2.0) + blackKeyNudge[offset] - MARGIN / 2.0;
-        var y = MARGIN;
+        var y = PADDING;
         ctx.fillRect(x, y, BLACK_KEY_WIDTH, BLACK_KEY_HEIGHT);
       }
     }
